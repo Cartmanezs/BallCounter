b6dfc9373a5d2717805e9057aa638c967a02ddef
Igor Palyvoda <tib.igor.mk@gmail.com>
2019-05-21 16:37:46 +0300

    Initial Commit

---
+++ Ball Counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ball Counter
+//
+//  Created by Ingvar on 5/21/19.
+//  Copyright © 2019 Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
